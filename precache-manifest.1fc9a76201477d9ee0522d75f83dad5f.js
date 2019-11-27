cbba69cbcfec090a5fd188bd1bc08239eee0fbe2
Carlos Alba <soradev4@gmail.com>
2019-11-27 11:10:31 -0800

    Updates

---
+++ precache-manifest.1fc9a76201477d9ee0522d75f83dad5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cfdf37fd7cf4a98c3024d6e8ed505b1",
+    "url": "/matandgrom/index.html"
+  },
+  {
+    "revision": "4416b5c243916a26a2e9",
+    "url": "/matandgrom/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "284560cf5ab6c7ef74d1",
+    "url": "/matandgrom/static/js/2.39ddf36a.chunk.js"
+  },
+  {
+    "revision": "4416b5c243916a26a2e9",
+    "url": "/matandgrom/static/js/main.16a30d4e.chunk.js"
+  },
+  {
+    "revision": "68614ecfcca1c67517cb",
+    "url": "/matandgrom/static/js/runtime-main.d1baf793.js"
+  }
+]);
